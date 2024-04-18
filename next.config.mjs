6509e892b1eb280094a0de052bdef3687caf6fd6
Daniel Purtov <daniel.purtov@gmail.com>
2024-04-18 10:16:51 +0200

    feat: POC for loading YAML files into Next.js as TypeScript modules using yaml-loader

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
     output: 'export',
     reactStrictMode: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        config.module.rules.push({
+            test: /\.ya?ml$/u,
+            use: 'yaml-loader',
+        });
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return config;
+    },
 };
 
 export default nextConfig;
