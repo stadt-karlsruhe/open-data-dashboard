9d34dcbf8577fe6bbb9716e0fb6194c87968849e
Daniel Purtov <daniel.purtov@gmail.com>
2024-04-19 10:35:30 +0200

    refactor: Move mock data to separate module, rename function

--- src/app/dataset/[resourceId]/page.tsx
+++ src/app/dataset/[resourceId]/page.tsx
@@ -1,29 +1,10 @@
 import DataTable from '@/components/visualization/DataTable';
-import { Resource } from '@/types/visualization';
+import { mockData } from '@/data/mockData';
 import { transformJson } from '@/transform';
-
-const mockData = [
-  {
-    id: '1',
-    name: 'Bevölkerung mit Hauptwohnung',
-    endpoint: 'https://transparenz.karlsruhe.de/datastore/dump/d8be5f4a-0788-4ee3-abe5-b36313ce3799?format=json',
-    skipFields: '^_id$',
-    renameFields: {
-      'mannlich (%)': 'Männlich (%)',
-      'weiblich (%)': 'Weiblich (%)',
-    },
-  },
-  {
-    id: '2',
-    name: 'Bevölkerung mit Hauptwohnung',
-    endpoint:
-      'https://transparenz.karlsruhe.de/api/3/action/datastore_search?resource_id=d8be5f4a-0788-4ee3-abe5-b36313ce3799&limit=450',
-  },
-] as Resource[];
 
 export default async function Page({ params: { resourceId } }: { params: { resourceId: string } }) {
   const resource = mockData.find((item) => item.id === resourceId);
-  const data = await getData(resource?.endpoint ?? '', resource?.skipFields, resource?.renameFields);
+  const data = await getJsonData(resource?.endpoint ?? '', resource?.skipFields, resource?.renameFields);
   return (
     <>
       <DataTable key={resourceId} data={data}></DataTable>
@@ -31,7 +12,7 @@ export default async function Page({ params: { resourceId } }: { params: { resou
   );
 }
 
-async function getData(endpoint: string, skipFields?: string, renameFields?: Record<string, string>) {
+async function getJsonData(endpoint: string, skipFields?: string, renameFields?: Record<string, string>) {
   const res = await fetch(endpoint);
   const json = (await res.json()) as unknown;
   return transformJson(json, skipFields, renameFields);
