b400d22bbca9fec9df820a7b444a071a5ee12035
Daniel Purtov <daniel.purtov@gmail.com>
2024-04-20 16:38:33 +0200

    refactor: Remove unused webpack config section

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
     output: 'export',
     reactStrictMode: true,
-    webpack: (config) => {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        config.resolve.alias.canvas = false;
-
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return config;
-    },
 };
 
 export default nextConfig;
