5594f424fd6f3a16ca436901ba258062978e2484
Chiara Scheurer <chiara.scheurer@gmx.de>
2024-04-21 14:15:38 +0200

    ci: Jest setup

---
+++ jest.config.ts
@@ -0,0 +1,29 @@
+import { type Config } from '@jest/types';
+
+const jestConfig: Config.InitialOptions = {
+    preset: 'ts-jest/presets/default-esm',
+
+    extensionsToTreatAsEsm: ['.ts', '.mts', '.json'],
+    moduleNameMapper: { '^(.{1,2}/.).m?js$': '$1' },
+
+    transform: {
+        '.test.m?ts$': [
+            'ts-jest',
+            {
+                useESM: true,
+                isolatedModules: false,
+            },
+        ],
+    },
+    collectCoverageFrom: ['<rootDir>/src/**/.ts'],
+    testEnvironment: 'node',
+
+    bail: true,
+    testRegex: '__tests__/.+\\.test\\.ts$',
+    coveragePathIgnorePatterns: ['<rootDir>/src/main.m?ts$', '.*.module.m?ts$', '<rootDir>/src/health/'],
+    coverageReporters: ['text-summary', 'html'],
+    errorOnDeprecated: true,
+    verbose: true,
+};
+
+export default jestConfig;
