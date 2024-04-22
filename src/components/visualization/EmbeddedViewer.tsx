7dbe064298c4ed746d78549f471ee7361a8cc1b3
Daniel Purtov <daniel.purtov@gmail.com>
2024-04-22 09:22:23 +0200

    fix: Build error due to `windows undefined`

--- src/components/visualization/EmbeddedViewer.tsx
+++ src/components/visualization/EmbeddedViewer.tsx
@@ -2,18 +2,28 @@
 
 import { useEffect, useState } from 'react';
 
-export default function PDFViewer({ source }: { source: string }) {
-  const [height, setHeight] = useState(window.innerHeight);
+const getWindowHeight = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    height,
+  };
+};
+const useWindowHeight = () => {
+  const [windowHeight, setWindowHeight] = useState(getWindowHeight());
   useEffect(() => {
     function handleResize() {
-      setHeight(window.innerHeight);
+      setWindowHeight(getWindowHeight());
     }
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+  return windowHeight;
+};
 
+export default function PDFViewer({ source }: { source: string }) {
+  const { height } = useWindowHeight();
   return (
     <div className="d-flex">
       <iframe src={source} width="100%" height={height} />
