b11ac9c1bbd7a19fa02767b3ffa6e32b6f21f6c2
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-02 11:05:54 +0200

    refactor: Remove redundant table property

--- src/components/visualization/Table.tsx
+++ src/components/visualization/Table.tsx
@@ -61,7 +61,6 @@ export default function Table({ columnNames, records }: { columnNames: string[];
         }}
         striped
         highlightOnHover
-        subHeader
         customStyles={customStyles}
       />
     </div>
