98afebf55d29926a708f4413ff56f7ddf649054e
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-02 10:58:58 +0200

    feat: Dynamically infer default locale

--- src/app/resource/[resourceId]/page.tsx
+++ src/app/resource/[resourceId]/page.tsx
@@ -1,8 +1,9 @@
+import { locales } from '@/locales';
 import { mockData } from '@/data/mockData';
 import { redirect } from 'next/navigation';
 
 export default function Page({ params: { resourceId } }: { params: { resourceId: string } }) {
-  redirect(`/en/resource/${resourceId}`);
+  redirect(`/${[...locales.values()][0]}/resource/${resourceId}`);
 }
 
 export function generateStaticParams() {
