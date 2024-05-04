d312772bcddca5448b10f44f06b5510b4f0338d5
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-04 10:56:30 +0200

    feat: Fallback to default language when missing translations for non-default languages

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,15 @@
 import { getRequestConfig } from 'next-intl/server';
 import { locales } from './locales';
+import { merge } from 'ts-deepmerge';
 import { notFound } from 'next/navigation';
 
 export default getRequestConfig(async ({ locale }: { locale: string }) => {
     if (!locales.has(locale)) {
         notFound();
     }
-    const t = (await import(`./messages/${locale}.json`)) as Record<string, never>;
+    const defaultMessages = (await import(`./messages/en.json`)) as IntlMessages;
+    const userMessages = (await import(`./messages/${locale}.json`)) as IntlMessages;
     return {
-        messages: t.default,
+        messages: merge(defaultMessages, userMessages),
     };
 });
