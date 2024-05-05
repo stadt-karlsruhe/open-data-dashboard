04081bd98eb8448e1cacba803c6d1d1dfdca0f75
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-05 17:22:06 +0200

    fix: Implement workaround for https://github.com/vercel/next.js/discussions/64660

--- src/app/[locale]/[...not-found]/page.tsx
+++
@@ -1,6 +0,0 @@
-// Necessary as workaround for a known Next.js bug: https://github.com/vercel/next.js/discussions/50034
-import { notFound } from 'next/navigation';
-
-export default function NotFoundLocale() {
-  notFound();
-}
