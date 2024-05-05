52c37d7acfdc6dc325da066d3d15a7ba94193a1d
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-05 15:12:30 +0200

    feat: Implement not-found page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useLocale, useTranslations } from 'next-intl';
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+import { locales } from '@/locales';
+
+const { Link } = createSharedPathnamesNavigation({
+  locales: [...locales.values()],
+});
+
+export default function NotFound() {
+  const t = useTranslations('NotFound');
+  const locale = useLocale();
+
+  return (
+    <div className="py-3 py-md-5 min-vh-100 d-flex justify-content-center align-items-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="text-center">
+              <h2 className="d-flex justify-content-center align-items-center gap-2 mb-4">
+                <span className="display-1 fw-bold">4</span>
+                <i className="bi bi-exclamation-circle-fill text-danger display-4"></i>
+                <span className="display-1 fw-bold bsb-flip-h">4</span>
+              </h2>
+              <h3 className="h2 mb-2">{t('title')}</h3>
+              <p className="mb-5">{t('subtitle')}</p>
+              <Link className="btn bsb-btn-5xl btn-dark badge px-5 fs-6 m-0" href="/">
+                {t('returnBtn')}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
