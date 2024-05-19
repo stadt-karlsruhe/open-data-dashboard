b2ced42cef745ec4c1382063133586ddd843e928
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-19 10:01:50 +0200

    refactor: Clean up Jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import { type Config } from '@jest/types';
 const jestConfig: Config.InitialOptions = {
     extensionsToTreatAsEsm: ['.ts', '.tsx'],
     moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
-
     transform: {
         '^.+\\.(t|j)sx?$': [
             '@swc/jest',
@@ -16,8 +15,6 @@ const jestConfig: Config.InitialOptions = {
     },
     collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
     coveragePathIgnorePatterns: ['src/(middleware|locales|i18n).ts'],
-    testEnvironment: 'node',
-
     bail: true,
     testRegex: String.raw`__tests__/.+\.test\.ts$`,
     errorOnDeprecated: true,
