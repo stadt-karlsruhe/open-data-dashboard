c562f52d6a3a286d4c6ba94ba908e667190b0592
Zerdados <Jan14717@gmail.com>
2024-05-25 12:38:13 +0200

    fix: delete unneeded css, simplify Legend

--- src/components/visualization/map/Legend.tsx
+++ src/components/visualization/map/Legend.tsx
@@ -6,15 +6,13 @@ import { useTranslations } from 'next-intl';
 export default function Legend({ labels }: { labels: Map<string, string> }) {
   const t = useTranslations('Legend');
   return (
-    <div className="bg-white card leaflet-bottom leaflet-left m-3" style={{ width: '200px', maxWidth: '50vw' }}>
-      <h6 className="card-header text-center">{t('title')}</h6>
+    <div className="bg-white card leaflet-bottom leaflet-left m-3 fs-6" style={{ width: '200px', maxWidth: '50vw' }}>
+      <div className="card-header text-center">{t('title')}</div>
       <div className="card-body">
         {[...labels.entries()].map(([label, color]) => (
           <div className="row" key={label}>
-            <div className="col-1 bi bi-circle-fill" style={{ color, fontSize: '16px' }} />
-            <div className="col-10" style={{ fontSize: '16px' }}>
-              {label}
-            </div>
+            <div className="col-1 bi bi-circle-fill" style={{ color }} />
+            <div className="col-10">{label}</div>
           </div>
         ))}
       </div>
