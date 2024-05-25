d8b1c6e694731a29e08ed2a57e14e6aebbf3da03
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-25 11:39:58 +0200

    fix: loop until 99 instead of 100 to match test description

--- __tests__/units/colors.test.ts
+++ __tests__/units/colors.test.ts
@@ -6,7 +6,7 @@ const cssVariableRegex = /var\(--[a-zA-Z0-9-]+\)/u;
 describe('colors', () => {
     it('should return a CSS variable expression for index-arguments from 0 to 99', () => {
         expect.hasAssertions();
-        for (let i = 0; i <= 100; i++) {
+        for (let i = 0; i < 100; i++) {
             const color = getColor(i);
             expect(color).not.toHaveLength(0);
             expect(color).toMatch(cssVariableRegex);
