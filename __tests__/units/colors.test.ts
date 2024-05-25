4f34ecbae7a311275939bff799258e81e3af6f60
Daniel Purtov <daniel.purtov@gmail.com>
2024-05-25 09:19:51 +0200

    fix: Incorrect file extension for colors test

---
+++ __tests__/units/colors.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from '@jest/globals';
+import { getColor } from '@/colors';
+
+describe('colors', () => {
+    it('should return a valid color for index-arguments from 0 to 99', () => {
+        expect.hasAssertions();
+        for (let i = 0; i <= 100; i++) {
+            const color = getColor(i);
+            expect(color).not.toHaveLength(0);
+        }
+    });
+});
