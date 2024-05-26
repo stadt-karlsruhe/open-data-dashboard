aba542f79b794d8a08bb27cced8c43ea567c988d
Daniel Purtov <93288603+danielptv@users.noreply.github.com>
2024-05-26 19:50:05 +0200

    feat: Implement option for setting default filters in configuration file (#87)
    * feat: Implement option for setting default filters
    
    * refactor: Use new Filter type in tests
    
    * feat: Validate that keys in renameFields are not skipped by skipFieldsRegex

--- src/schema.ts
+++ src/schema.ts
@@ -34,10 +34,24 @@ const AxisPairSchema = z
     })
     .strict();
 
+const DefaultFilterSchema = z.union([
+    z.string(),
+    z
+        .object({
+            min: z.number().transform(String).optional(),
+            max: z.number().transform(String).optional(),
+        })
+        .strict()
+        .refine((data) => data.min !== undefined || data.max !== undefined, {
+            message: "At least one of 'min' or 'max' must be defined",
+        }),
+]);
+
 export const TransformableResourceSchema = BaseResourceSchema.extend({
     type: z.union([z.literal('JSON'), z.literal('CSV')]),
     skipFieldsRegEx: z.string().optional(),
     renameFields: z.record(z.string()).optional(),
+    defaultFilters: z.record(z.string(), DefaultFilterSchema).optional(),
     visualizations: z
         .object({
             barChart: z
@@ -50,7 +64,21 @@ export const TransformableResourceSchema = BaseResourceSchema.extend({
         })
         .strict()
         .default({ table: {} }),
-}).strict();
+})
+    .strict()
+    .refine(
+        (data) => {
+            if (data.skipFieldsRegEx && data.renameFields) {
+                const regex = new RegExp(data.skipFieldsRegEx, 'u');
+                return !Object.keys(data.renameFields).some((key) => regex.test(key));
+            }
+            return true;
+        },
+        {
+            message: 'skipFieldsRegEx should not match any keys in renameFields',
+            path: ['skipFieldsRegEx'],
+        },
+    );
 
 export const ResourceSchema = z.union([GeoJSONResourceSchema, TransformableResourceSchema, EmbeddedResourceSchema]);
 
@@ -72,3 +100,4 @@ export type AxisPair = z.infer<typeof AxisPairSchema>;
 export type TransformableResource = z.infer<typeof TransformableResourceSchema>;
 export type Resource = z.infer<typeof ResourceSchema>;
 export type Configuration = z.infer<typeof ConfigurationSchema>;
+export type Filter = z.infer<typeof DefaultFilterSchema>;
