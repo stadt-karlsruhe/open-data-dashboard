4013000de1d0c0e082f971d3ca506e7d368ed715
Abdullah Atak <abdullahatak@ive.de>
2024-06-06 07:49:39 +0200

    feat: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,27 @@
+import { useTranslations } from 'next-intl';
+
 export default function Footer() {
+  const t = useTranslations('Footer');
   return (
     <div>
-      <div>hallo</div>
+      <div>
+        <footer className="bg-black py-2 d-flex flex-wrap">
+          <div className="container-sm d-flex flex-wrap justify-content-around">
+            <a
+              href="#"
+              className="lead link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+            >
+              {t('imprint')}
+            </a>
+            <a
+              href="#"
+              className="lead link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+            >
+              {t('dataProtection')}
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
