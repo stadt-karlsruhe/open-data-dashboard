8b0ffa063b91f841c794949eb50d807f0ca60e20
Abdullah Atak <abdullahatak@live.de>
2024-06-09 11:08:46 +0200

    feat: integrate Footer in layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import { useTranslations } from 'next-intl';
 
-export default function Footer() {
+export default function Footer({ className }: { className?: string }) {
   const t = useTranslations('Footer');
   return (
-    <footer className="bg-black py-2 d-flex flex-wrap">
+    <footer className={`footer bg-black py-2 d-flex flex-wrap ${className ?? ''}`}>
       <div className="container-sm d-flex flex-wrap justify-content-around">
         <a
           href="#"
