000e795ff34901c2dbbb1c97c15ff889af1bc3c4
Daniel Purtov <daniel.purtov@gmail.com>
2024-06-15 16:40:35 +0200

    feat: Adjust ChartTableFilter icon based on customer feedback

--- src/components/visualization/chart-table-filter/ChartTableFilterHeader.tsx
+++ src/components/visualization/chart-table-filter/ChartTableFilterHeader.tsx
@@ -43,7 +43,8 @@ export function ChartTableFilterHeader({
         title={isCurrentEventKey ? t('collapseTooltipCollapse') : t('collapseTooltipExpand')}
         onClick={decoratedOnClick}
       >
-        {isCurrentEventKey ? <i className="bi bi-caret-up-square"></i> : <i className="bi bi-caret-down-square"></i>}
+        <i className="bi bi-funnel-fill fs-5" />
+        {isCurrentEventKey ? <i className="bi bi-caret-up" /> : <i className="bi bi-caret-down" />}
       </button>
     </ClearableInputGroup>
   );
