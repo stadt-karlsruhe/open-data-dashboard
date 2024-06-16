e0f2a01d5776b6e0efc43604fb580101143b5002
Zerdados <Jan14717@gmail.com>
2024-06-16 17:24:09 +0200

    feat: add cypress

---
+++ cypress/e2e/spec.home.cy.ts
@@ -0,0 +1,26 @@
+const searchbar = 'input[placeholder="Search"]';
+
+describe('homepage tests', () => {
+    it('visiting root should redirect to homepage', () => {
+        cy.visit('');
+        cy.url().should('include', '/home');
+    });
+    it('homepage should contain title', () => {
+        cy.visit('');
+        cy.get('title').contains('Open Data Dashboard');
+    });
+    it('homepage should contain searchbar', () => {
+        cy.visit('');
+        cy.get(searchbar);
+    });
+    it('searchbar should find entry', () => {
+        cy.visit('');
+        cy.get(searchbar).type('Wohnberechtigte');
+        cy.contains('Wohnberechtigte Bevölkerung');
+    });
+    it('searchbar should find entry with typos', () => {
+        cy.visit('');
+        cy.get(searchbar).type('Wohnerecht');
+        cy.contains('Wohnberechtigte Bevölkerung');
+    });
+});
