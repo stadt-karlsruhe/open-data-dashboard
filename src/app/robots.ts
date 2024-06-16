06f3d9b4c477f5326855d9f487bcc86fd37ba115
Daniel Purtov <daniel.purtov@gmail.com>
2024-06-16 07:39:12 +0200

    fix. Adjust robots.txt URLs

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
             {
                 userAgent: '*',
                 allow: '/',
-                disallow: ['/resource/*'],
+                disallow: ['/embed/resource/*', '/embed/dashboard/*'],
             },
         ],
     };
