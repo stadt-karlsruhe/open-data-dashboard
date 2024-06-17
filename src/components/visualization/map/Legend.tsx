708cace0c4ef8fd88683007cc9d8ca3d47824da1
Zerdados <Jan14717@gmail.com>
2024-06-17 20:13:04 +0200

    fix: dirty legend fix

--- src/components/visualization/map/Legend.tsx
+++ src/components/visualization/map/Legend.tsx
@@ -1,11 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+import L from 'leaflet';
 import { useTranslations } from 'next-intl';
 
 export default function Legend({ labels }: { labels: Map<string, string> }) {
   const t = useTranslations('Legend');
+  const divLegend = useRef(null);
+  useEffect(() => {
+    if(divLegend.current !== null){
+        L.DomEvent.disableScrollPropagation(divLegend.current);
+    }
+  })
   return (
-    <div
-      className="bg-white card leaflet-bottom leaflet-left position-absolute m-3 fs-6"
-      style={{ width: '200px', maxWidth: '50vw', maxHeight: '200px' }}
+    <div ref={divLegend}
+      className="bg-white card position-absolute m-3 fs-6"
+      style={{ width: '200px', maxWidth: '50vw', maxHeight: '200px', zIndex: 400, bottom: '6px', left: '6px' }}
     >
       <div className="card-header text-center">{t('title')}</div>
       <div className="card-body overflow-y-auto">
