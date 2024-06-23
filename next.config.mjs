4885ae30d7da43783001d614158f37eb37a85017
Daniel Purtov <daniel.purtov@gmail.com>
2024-06-23 11:55:40 +0200

    feat: Switch to licensed homepage image

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                hostname: 'rp.baden-wuerttemberg.de',
-            },
-        ],
-    },
 };
 
 export default withNextIntl(nextConfig);
--- src/app/[locale]/(main)/home/page.tsx
+++ src/app/[locale]/(main)/home/page.tsx
@@ -1,11 +1,16 @@
 import DashboardContents from '@/components/dashboard-resource/dashboard/DashboardContents';
 import ErrorComponent from '@/components/error-handling/ErrorComponent';
 import Image from 'next/image';
+import KarlsruheCastle from '@/public/Karlsruhe_Palace.jpg';
+import Link from 'next/link';
 import Search from '@/components/search/Search';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import { getTranslations } from 'next-intl/server';
 import { getValidatedConfiguration } from '@/schemas/validate';
 
 export default async function Home() {
   const { success, configuration, error } = await getValidatedConfiguration();
+  const t = await getTranslations('Homepage');
   if (!success) {
     return <ErrorComponent type="configurationError" error={error} />;
   }
@@ -14,12 +19,10 @@ export default async function Home() {
   return (
     <div className="d-flex flex-column flex-grow-1">
       <div className="position-relative w-100" style={{ height: '500px' }}>
-        {/* TODO: Acquire licensed picture or add attribution */}
-        {/* Source: https://rp.baden-wuerttemberg.de/rps/presse/artikel/bau-und-kunstdenkmalpflege-schloesser-und-gaerten-in-baden-wuerttemberg/ */}
         <Image
           className="object-fit-cover"
-          src="https://rp.baden-wuerttemberg.de/fileadmin/_processed_/8/0/csm_AdobeStock_516119904_Schloss_Karlsruhe_Markus_Mainka_7fd664f58b.jpeg"
-          alt="Karlsruher Schloss"
+          src={KarlsruheCastle as StaticImport}
+          alt={t('imageAlt')}
           priority
           fill
           sizes="1700px"
@@ -28,8 +31,17 @@ export default async function Home() {
           configuration={configuration}
           id="homepage-search"
           className="position-absolute w-100 start-50 px-2"
-          style={{ bottom: '20%', transform: 'translateX(-50%)', maxWidth: '600px' }}
+          style={{ bottom: '35%', transform: 'translateX(-50%)', maxWidth: '600px' }}
         />
+        <div
+          className="position-absolute bottom-0 end-0 bg-white d-flex p-1"
+          style={{ fontSize: '0.75rem', opacity: '80%' }}
+        >
+          &copy;
+          <Link href="https://www.karlsruhe.de/" target="_blank" className="nav-link ms-1">
+            Stadt Karlsruhe, Roland Fränkle
+          </Link>
+        </div>
       </div>
       {homepage && <DashboardContents dashboard={homepage} configuration={configuration} className="m-2" />}
     </div>
